ec817cd2f7da26c5e9963384c7c49291f6cf9f6e
Mureed Hussain <imhussain0086@gmail.com>
2022-02-02 21:31:28 +0500

    update
    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(0);
+  const getData = () => {
+    console.log("Value");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Please input your data here</h1>
+      <input type="text" placeholder="Input Your data" required></input>
+      <button type="submit" onClick={getData}>
+        ADD
+      </button>
     </div>
   );
 }
